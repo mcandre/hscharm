d5ca1649488c6e869cdeec6dcb3260a66fe105a9
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2013-11-02 14:50:25 -0400

    tabs

--- ddr.hs
+++ ddr.hs
@@ -6,64 +6,63 @@ import Control.Monad (when)
 import Text.Printf (printf)
 
 data Game = Game {
-		score :: Int,
-		delta :: Int
-	}
+  score :: Int,
+  delta :: Int
+  }
 
 drawScore :: Int -> IO ()
 drawScore s = do
-	moveCursor 0 2
-	hCenterString $ printf "SCORE %04d" s
+  moveCursor 0 2
+  hCenterString $ printf "SCORE %04d" s
 
 drawArrow :: Key -> IO ()
 drawArrow a = do
-	moveCursor 0 4
+  moveCursor 0 4
 
-	hCenterString $ case a of
-		KeyUp -> "^"
-		KeyDown -> "v"
-		KeyRight -> ">"
-		KeyLeft -> "<"
+  hCenterString $ case a of
+    KeyUp -> "^"
+    KeyDown -> "v"
+    KeyRight -> ">"
+    KeyLeft -> "<"
+    _ -> ""
 
 drawHoot :: Bool -> IO ()
 drawHoot b = do
-	moveCursor 0 6
+  moveCursor 0 6
 
-	hCenterString $ if b
-		then "GOOD!"
-		else "BAD! "
+  hCenterString $ if b then "GOOD!" else "BAD! "
 
 loop :: Game -> IO ()
 loop g = do
-	drawScore $ score g
+  drawScore $ score g
 
-	arrow <- runRVar (choice [KeyUp, KeyDown, KeyRight, KeyLeft]) DevRandom
+  arrow <- runRVar (choice [KeyUp, KeyDown, KeyRight, KeyLeft]) DevRandom
 
-	drawArrow arrow
+  drawArrow arrow
 
-	k <- getKey
+  k <- getKey
 
-	when (k `notElem` [KeyEscape, KeyQ])
-		(do
-			let match = arrow == k
+  when (k `notElem` [KeyEscape, KeyQ])
+    (do
+       let match = arrow == k
 
-			drawHoot match
+       drawHoot match
 
-			let g' = g { score = score g + (if match then 1 else -1) }
+       let g' = g { score = score g + (if match then 1 else -1) }
 
-			loop g')
+       loop g')
 
 main :: IO ()
 main = do
-	startCharm
+ startCharm
 
-	hCenterString "DDR: How fast can you play?"
+ hCenterString "DDR: How fast can you play?"
 
-	let g = Game {
-			score = 0,
-			delta = 0
-		}
+ let g = Game {
+       score = 0,
+       delta = 0
+       }
 
-	loop g
+ loop g
 
-	endCharm
+ endCharm
